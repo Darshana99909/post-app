861df26638ec48bb5ba75ea92ec5268e5a50247e
Darshana99909 <darshana99909@gmail.com>
2025-07-13 22:10:47 +0530

    project created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Addpost from './components/Addpost';
+import Searchpost from './components/Searchpost';
+import Deletepost from './components/Deletepost';
+import Viewall from './components/Viewall';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Addpost/>}/>
+      <Route path="/Searchpost" element={<Searchpost/>}/>
+      <Route path="/Deletepost" element={<Deletepost/>}/>
+      <Route path="/Viewall" element={<Viewall/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Addpost.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Addpost = () => {
+  return (
+    <div>
+       <Navbar/>
+        <div className="container">
+            
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="bg-light p-4 rounded shadow">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-lab">User id</label>
+                            <input className="form-control"></input>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Id</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">title</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">body</label>
+                            <textarea name="" id="" className="form-control"></textarea>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-primary">submit</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Addpost
